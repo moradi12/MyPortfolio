8f9631b3f9addb4975b985969682012f19f29ca4
moradi12 <tamirmoradi@gmail.com>
2024-07-10 16:06:38 +0300

    Increase font size and change color to white for email, LinkedIn, and GitHub links in Contact Me section

--- src/Components/ContactMe/ConactMe.jsx
+++ src/Components/ContactMe/ConactMe.jsx
@@ -16,6 +16,7 @@ const ContactMe = () => {
                 href="mailto:TamirMoradi@Gmail.com"
                 target="_blank" 
                 rel="noopener noreferrer"
+                className="email-link"
               >
                 TamirMoradi@Gmail.com
               </a>
@@ -28,6 +29,7 @@ const ContactMe = () => {
                 href="https://github.com/moradi12" 
                 target="_blank" 
                 rel="noopener noreferrer"
+                className="github-link"
               >
                 github.com/moradi12
               </a>
@@ -40,6 +42,7 @@ const ContactMe = () => {
                 href="https://www.linkedin.com/in/tamir-moradi-1a62b0260/" 
                 target="_blank" 
                 rel="noopener noreferrer"
+                className="linkedin-link"
               >
                 LinkedIn/Tamir Moradi
               </a>
